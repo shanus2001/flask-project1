5246e2ffcfbca5827e3d5d930c2c17d09975355e
shanu <shanuchoudharyvikas@gmail.com>
2024-04-01 15:39:26 +0530

    secday

--- main.py
+++ main.py
@@ -18,10 +18,10 @@ def contactus():
     return render_template("contact.html")
 @app.route("/savethis",methods=["post"])
 def savethisdata():
-    if request.method=="post":
+    if request.method=="POST":
         titles=request.form.get("title")
         message=request.form.get("msg")
         print(titles,message)
-    return "data saved success fully"
+        return "data saved success fully"
 if __name__ =="__main__":
     app.run(debug=True)
