b849b0cb5ba94b5258f02c50c3d3ed6aa15a45da
shanu <shanuchoudharyvikas@gmail.com>
2024-04-03 15:05:08 +0530

    fourday

--- main.py
+++ main.py
@@ -1,18 +1,18 @@
 from flask import Flask
 from flask import render_template,request,redirect
 from flask_sqlalchemy import SQLAlchemy
-import mysql.connector
-conn=mysql.connector.connect(host="localhost",username="root",password="Yamaha@8763",database="flaskdata")
-cuser=conn.cursor()
+# import mysql.connector
+# conn=mysql.connector.connect(host="localhost",username="root",password="Yamaha@8763",database="flaskdata")
+# cuser=conn.cursor()
 app=Flask(__name__)
-# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///firstdb.db"
-# db=SQLAlchemy(app)
-# class Contactus(db.Model):
-#     myid=db.Column(db.Integer,primary_key=True,autoincrement=True)
-#     mytitle=db.Column(db.String(120))
-#     mymessage=db.Column(db.Text)
-# with app.app_context():    
-#    db.create_all()    
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///firstdb.db"
+db=SQLAlchemy(app)
+class Contactus(db.Model):
+    myid=db.Column(db.Integer,primary_key=True,autoincrement=True)
+    mytitle=db.Column(db.String(120))
+    mymessage=db.Column(db.Text)
+with app.app_context():    
+   db.create_all()    
 # @app.route("/")
 # def basefile():
 #     return render_template("base.html")
@@ -24,9 +24,10 @@ def aboutuss():
     return render_template("myap/aboutus.html")
 @app.route("/services")
 def myservices():
-    cuser.execute("select * from flasksave")
-    data=cuser.fetchall()
-    return render_template("myap/services.html",mydata=data)
+    # cuser.execute("select * from flasksave")
+    # data=cuser.fetchall()
+    # return render_template("myap/services.html",mydata=data)
+    return render_template("myap/services.html")
 @app.route("/contact")
 def contactus():
     return render_template("contact.html")
@@ -35,16 +36,16 @@ def savethisdata():
     if request.method=="POST":
         titles=request.form.get("title")
         message=request.form.get("msg")
-        cuser.execute(f"insert into flasksave values('{titles}','{message}')")
-        conn.commit()
-        # data=Contactus(mytitle=titles,mymessage=message)
-        # db.session.add(data)
-        # db.session.commit()
+        # cuser.execute(f"insert into flasksave values('{titles}','{message}')")
+        # conn.commit()
+        data=Contactus(mytitle=titles,mymessage=message)
+        db.session.add(data)
+        db.session.commit()
         return redirect("/contact")
-@app.route("/deletethisdata/<a>",methods=["POST"])
-def deletedata(a):
-    cuser.execute(f" delete from flasksave where title='{a}'")
-    return redirect("/services")
+# @app.route("/deletethisdata/<a>",methods=["POST"])
+# def deletedata(a):
+#     cuser.execute(f" delete from flasksave where title='{a}'")
+#     return redirect("/services")
 
 
 if __name__ =="__main__":
